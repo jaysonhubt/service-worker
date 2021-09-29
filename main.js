5a27069978796f38466dd482a7ce545c05d42564
Sonth <sonth@miichisoft.net>
2021-09-29 10:55:45 +0700

    Fix Notification

--- main.js
+++ main.js
@@ -9,15 +9,34 @@ if (window.Notification && Notification.permission === "granted") {
 		});
 	}
 } else if (window.Notification && Notification.permission === "denied" && Notification.permission === "default") {
-	Notification.requestPermission().then(function (status) {
-        // If the user said okay
-        if (status === "granted") {
-        	alert("Hi! Allowed");
-      }
+	try {
+		Notification.requestPermission().then(function (status) {
+			// If the user said okay
+			if (status === "granted") {
+				alert("Hi! Allowed");
+			}
 
-        else {
-        	alert("Hi! Denied");
-        }
-    });
+			else {
+				alert("Hi! Denied");
+			}
+		});
+	} catch (error) {
+		// Safari doesn't return a promise for requestPermissions and it
+		// throws a TypeError. It takes a callback as the first argument
+		// instead.
+		if (error instanceof TypeError) {
+			Notification.requestPermission(() => {
+				if (status === "granted") {
+					alert("Hi! Allowed");
+				}
+
+				else {
+					alert("Hi! Denied");
+				}
+			});
+		} else {
+			throw error;
+		}
+	}
 }
 console.log(Notification.permission)
