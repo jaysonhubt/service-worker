aa4acc5b4911e784360233747068fa96c588efb1
Sonth <sonth@miichisoft.net>
2021-09-29 18:14:35 +0700

    Fix Notification

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
 if (window.Notification && Notification.permission === "granted") {
 	if ('serviceWorker' in navigator) {
 		navigator.serviceWorker.register('service_worker.js')
-		.then(reg => {
+		.then(function (reg) {
 			console.log('Registered service worker');
-			// reg.addEventListener('notificationclick', function(event, window) {
-			// 	event.notification.close();
-			// 	if (event.action === 'archive') {
-			// 		console.log('archive')
-			// 	} else if (event.action === 'test') {
-			// 		// Main body of notification was clicked
-			// 		alert('test')
-			// 	} else {
-			// 		// Main body of notification was clicked
-			// 		window.focus()
-			// 	}
-			// });
-			reg.showNotification('title', { body: 'text', actions: [{action: 'archive', title: 'Archive'}, {action: 'test', title: 'Test'}] });
-			console.log(reg.getNotifications())
+			reg.showNotification(
+				'着信-Son',
+				{
+					body: 'Content',
+					actions: [
+						{
+							action: 'button1',
+							title: 'Button 1'
+						},
+						{
+							action: 'button2',
+							title: 'Button 2'
+						}
+					]
+				}
+			);
+			reg.addEventListener('notificationclick', function () {
+				console.log('notificationclick')
+			})
 		}).catch(err => {
 			console.log('Register service worker failed', err);
 		});
