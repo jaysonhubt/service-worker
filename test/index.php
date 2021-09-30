5f3f1665401cd3f8889b81044c3df455b4b07197
Sonth <sonth@miichisoft.net>
2021-09-30 08:29:39 +0700

    Fix Notification

--- test/index.php
+++ test/index.php
@@ -3,7 +3,6 @@
 		<link rel="icon" href="/favicon.ico">
 	</head>
 	<body>
-        <script src="/service_worker.js<?php echo '?=' . time(); ?>"></script>
 		<script src="/main.js<?php echo '?=' . time(); ?>"></script>
 	</body>
 </html>
